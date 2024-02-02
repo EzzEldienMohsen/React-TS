1dd848d5cbafc26b72d3903939e6f6e41002cb6e
Ezz dghedy <ezzmohsend@gmail.com>
2024-02-02 17:13:37 +0200

    inital structure

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,13 @@
+const CourseGoal = (props) => {
+  return (
+    <article>
+      <div>
+        <h1>Title</h1>
+        <p>DESCRIPTION</p>
+      </div>
+      <button>DELETE</button>
+    </article>
+  );
+};
+
+export default CourseGoal;
