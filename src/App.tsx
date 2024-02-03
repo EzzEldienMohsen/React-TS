a1939205186ee4c349eb326b7e062ac2ac9a1fe1
Ezz dghedy <ezzmohsend@gmail.com>
2024-02-04 00:28:40 +0200

    finalized

--- src/App.tsx
+++ src/App.tsx
@@ -11,24 +11,13 @@ export interface CourseGoals {
 }
 export default function App() {
   const [goals, setGoals] = React.useState<CourseGoals[]>([]);
-  const handleAddGoal = (): void => {
-    setGoals((prevGoals) => {
-      return [
-        ...prevGoals,
-        {
-          title: 'mastering react with ts',
-          description: 'taking it step by step',
-          id: Math.random(),
-        },
-      ];
-    });
-  };
+
   return (
     <main>
       <Header image={{ src: goalImg, alt: 'course goals' }}>
         <h1>Course goals</h1>
       </Header>
-      <NewGoal />
+      <NewGoal setGoals={setGoals} />
       <CourseGoalsList goals={goals} setGoals={setGoals} />
     </main>
   );
